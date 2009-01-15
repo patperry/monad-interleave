9a29c0e92bee48f6e7d75798222a8f1771dba2fc
Patrick Perry <patperry@stanford.edu>
2009-01-15 11:30:46 -0800

    initial import

---
+++ Control/Monad/Interleave.hs
@@ -0,0 +1,16 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module     : Control.Monad.Interleave
+-- Copyright  : Copyright (c) , Patrick Perry <patperry@stanford.edu>
+-- License    : BSD3
+-- Maintainer : Patrick Perry <patperry@stanford.edu>
+-- Stability  : experimental
+--
+-- Monads with an unsaveInterleaveIO-like operation.
+--
+
+module Control.Monad.Interleave (
+    ) where
+
+
+
