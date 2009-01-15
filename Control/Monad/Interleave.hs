83dfa52455a4a23957764db680b7626278fed0f8
Patrick Perry <patperry@stanford.edu>
2009-01-15 11:38:53 -0800

    add implementation

--- Control/Monad/Interleave.hs
+++ Control/Monad/Interleave.hs
@@ -10,7 +10,26 @@
 --
 
 module Control.Monad.Interleave (
+    MonadInterleave(..),
     ) where
 
+import Control.Monad.ST
+import qualified Control.Monad.ST.Lazy as Lazy
+import System.IO.Unsafe
 
+-- | Monads that have an operation like 'unsafeInterleaveIO'.
+class Monad m => MonadInterleave m where
+    -- | Get the baton from the monad without doing any computation.
+    unsafeInterleave :: m a -> m a
 
+instance MonadInterleave IO where
+    unsafeInterleave = unsafeInterleaveIO
+    {-# INLINE unsafeInterleave #-}
+    
+instance MonadInterleave (ST s) where
+    unsafeInterleave = unsafeInterleaveST
+    {-# INLINE unsafeInterleave #-}
+    
+instance MonadInterleave (Lazy.ST s) where
+    unsafeInterleave = Lazy.unsafeInterleaveST
+    {-# INLINE unsafeInterleave #-}
